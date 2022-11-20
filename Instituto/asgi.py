225f15cb75b4fdc674ff406415712bf1ccfe8af6
Anurag Shakya <officialanuragdev@gmail.com>
2022-11-20 16:19:09 +0530

    UserToUserRealTimeChat

--- Instituto/asgi.py
+++ Instituto/asgi.py
@@ -17,9 +17,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 
 import os
-
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from notifications.consumers import UserToUserRealTime
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -28,5 +33,13 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path("usertouser/", UserToUserRealTime.as_asgi()),
+            ])
+        )
+    ),
     # Just HTTP for now. (We can add other protocols later.)
 })
